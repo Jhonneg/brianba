5c1647aed56d07dd2a185ba0f362efe48e238fc5
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-05-12 15:09:45 -0300

    added redis to docker

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,3 +1,22 @@
+import jwt from "jsonwebtoken";
+import redis from "redis";
+
+(async () => {
+  const redisClient = redis.createClient({
+    url: process.env.REDIS_URI,
+  });
+  redisClient.on("error", console.error);
+
+  await redisClient.connect();
+
+  await redisClient.set("key", "value");
+  const value = await redisClient.get("key");
+
+  console.log(value);
+})();
+
+const token = jwt.sign({ foo: "bar" }, "shhhhh");
+
 const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
@@ -20,17 +39,36 @@ const handleSignin = (db, bcrypt, req, res) => {
         Promise.reject("Wrong credentials");
       }
     })
-    .catch((err) => res.status(400).json("Wrong credentials"));
+    .catch((err) => Promise.reject("Wrong credentials"));
 };
 function getAuthTokenId() {
   console.log("auth ok");
+}
+function signToken(email) {
+  const jwtPayload = { email };
+  return jwt.sign(jwtPayload, "JWT-SECRET", { expiresIn: "2days" });
+}
+function setToken(key, value) {
+  return Promise.resolve(redisClient.set(key, value));
+}
+function createSessions(user) {
+  const { email, id } = user;
+  const token = signToken(email);
+  return setToken(token, id)
+    .then(() => {
+      return { success: "true", userId: id, token };
+    })
+    .catch(console.log);
 }
 const signinAuthentication = (db, bcrypt) => (req, res) => {
   const { authorization } = req.headers;
   return authorization
     ? getAuthTokenId()
     : handleSignin(db, bcrypt, req, res)
-        .then((data) => res.json(data))
+        .then((data) =>
+          data.id && data.email ? createSessions(data) : Promise.reject(data)
+        )
+        .then((session) => res.json(session))
         .catch((err) => res.status(400).json(err));
 };
 
